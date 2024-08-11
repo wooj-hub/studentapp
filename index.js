ab6e7b45803f6965fd015c644289cf01a8c8c647
wooj-hub <wjddnwls0803@naver.com>
2024-08-11 11:32:11 +0900

    수정

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 app.listen(port);
 app.get("/api/lunch", (req, res) =>
-  res.json({ menu: "떡볶이", store: "신전떡볶이", price: 15000 })
+  res.json({ menu: "감자", store: "신전떡볶이", price: 15000 })
 );
 
 app.get("/api/dinner", (req, res) =>
